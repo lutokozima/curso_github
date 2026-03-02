e744e5c56fba2b495d5d3c5ad735ddf87800d08b
devluciana <lycytkz@gmail.com>
2026-03-02 15:31:57 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome) {
-    return nome    
+    return nome;  
 }
+
+function mostraSobreNomeSobre(sobreNome) {
+    return sobreNome;    
+}
+
+//primeiro sistema da home
--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 // trabalhando no login
 
 //finalizando sistema de login
+function login(nome, email){
+    return "Bem vindo";
+}
+
+
+//seja bem vindo
+//sistema de login completo
+//retornamos o nome do usuario quando logado
