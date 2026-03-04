2fa4dfa1ea2b41c9be733c75f41c00ded8f2f26b
devluciana <lycytkz@gmail.com>
2026-03-04 13:45:48 -0300

    Adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -3,3 +3,9 @@ function signUp(email){
 }
 
 //pegando o email e cadastrando na plataforma
+
+function mostraMensagem() {
+    return 'Bem vindo ao sistema';    
+}
+
+//adicionando funcao de mensagem
